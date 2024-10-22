5ee00dcc78134506ce30c7ba0b3bd9ce1eedb276
Krisna-Bou <krisna.bou@hotmail.com>
2024-10-22 18:30:34 +1000

    Check mobile

--- js/script.js
+++ js/script.js
@@ -1,27 +1,17 @@
-// Get the headers position from the top of the page, set to 90% of the viewport height
-var startY = window.innerHeight * 0.1; // This sets startY to 90vh
-var previous = window.scrollY;
+// var previous = window.scrollY;
+// $(window).scroll(function(){
+//     checkY();
+// });
 
+// function checkY(){
+//     var current = window.scrollY;
+//     if (previous >= current) {
+//         $('.masthead').slideDown();
+//     } else {
+//         $('.masthead').slideUp();
+//     }
+//     previous = current;
+// }
 
-$(window).scroll(function(){
-    checkY();
-});
-
-function checkY(){
-    var current = window.scrollY;
-    if( $(window).scrollTop() > startY ){
-        $('.masthead-top').slideUp(100);
-        if (previous > current && $(window).scrollTop() >  window.innerHeight * 0.1) {
-            $('.masthead-bottom').slideDown();
-        } else {
-            $('.masthead-bottom').slideUp();
-        }
-    }else{
-        $('.masthead-top').slideDown(); 
-        $('.masthead-bottom').slideUp(); // Hide the masthead if scrolled above startY
-    }
-    previous = current;
-}
-
-// Do this on load just in case the user starts halfway down the page
-checkY();
+// // Do this on load just in case the user starts halfway down the page
+// checkY();
