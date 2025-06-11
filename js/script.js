fbe9e1db205956da206d8f7aedba7507e5eef250
Krisna-Bou <krisna.bou@hotmail.com>
2025-06-11 14:15:15 +1000

    Design Update

--- js/script.js
+++ js/script.js
@@ -15,3 +15,17 @@
 
 // // Do this on load just in case the user starts halfway down the page
 // checkY();
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var previous = window.pageYOffset;
+window.onscroll = function () {
+    var current = window.pageYOffset;
+
+    //Find if the user is scrolling down or up on each scroll
+    if (previous > current) {
+        document.getElementById("header").style.top = "0";
+    } else {
+        document.getElementById("header").style.top = "-200px";
+    }
+    previous = current;
+}
