9eae10fae11f0f3449b8fdbd7c16bc483e2d868b
Krisna-Bou <krisna.bou@hotmail.com>
2025-06-13 11:03:05 +1000

    Fix nav bar hiding mobile

--- js/script.js
+++ js/script.js
@@ -1,22 +1,3 @@
-// var previous = window.scrollY;
-// $(window).scroll(function(){
-//     checkY();
-// });
-
-// function checkY(){
-//     var current = window.scrollY;
-//     if (previous >= current) {
-//         $('.masthead').slideDown();
-//     } else {
-//         $('.masthead').slideUp();
-//     }
-//     previous = current;
-// }
-
-// // Do this on load just in case the user starts halfway down the page
-// checkY();
-
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var previous = window.pageYOffset;
 window.onscroll = function () {
     var current = window.pageYOffset;
@@ -25,7 +6,9 @@ window.onscroll = function () {
     if (previous > current) {
         document.getElementById("header").style.top = "0";
     } else {
-        document.getElementById("header").style.top = "-200px";
+        if (current > 150) {
+            document.getElementById("header").style.top = "-200px";
+        }
     }
     previous = current;
 }
