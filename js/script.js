5e3ee3c01d3801f0820e0192db90007d50529ba0
Krisna-Bou <krisna.bou@hotmail.com>
2025-06-13 11:05:57 +1000

    Fixing script

--- js/script.js
+++ js/script.js
@@ -6,9 +6,12 @@ window.onscroll = function () {
     if (previous > current) {
         document.getElementById("header").style.top = "0";
     } else {
-        if (current > 150) {
+        if (current > 200) {
             document.getElementById("header").style.top = "-200px";
         }
     }
     previous = current;
+    if (current < 200) {
+        document.getElementById("header").style.top = "0";
+    }
 }
